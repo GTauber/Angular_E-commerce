c323e4e6c69223b00500abeb831d5dfb67f66527
c1326678 Gabriel Tauber De Andrade Figueiredo <c1326678@interno.bb.com.br>
2022-07-13 09:47:44 -0300

    Basics Finalized

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
-import { Recipe } from './recipe.model';
+import { Recipe } from '../recipe.model';
 
 @Component({
-  selector: 'app-recipes',
-  templateUrl: './recipes.component.html',
-  styleUrls: ['./recipes.component.css']
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
 })
-export class RecipesComponent implements OnInit {
+export class RecipeListComponent implements OnInit {
+
   recipes: Recipe[] = [
     new Recipe('Rebecca', 'A mais linda de todas', 'https://lh3.googleusercontent.com/pw/AM-JKLV3djWOiIuZx5deBcBFkdtpqZ6CpvfKckM48w21XPW_6KboiFPIGdrq1tspWIf16tTTFWXGF5GGdSCuyUIkAidwr6yuwfV19qNyt1KkFbSQNsju1tuTP-NMllabYCh_rM3_fGR56OBsbKSGszgHAX1rbQ=w736-h980-no?authuser=0')
   ];
